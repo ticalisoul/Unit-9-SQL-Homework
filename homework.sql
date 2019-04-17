f89614438f75625b48875c04e8c5eec072407690
Danny L <wlou@ucdavis.edu>
2019-04-17 15:39:08 -0700

    updated sql file

--- homework.sql
+++ homework.sql
@@ -29,3 +29,40 @@ select distinct last_name, count(*) from actor
 group by last_name
 having count(*) > 1;
 #4c
+update actor
+set first_name = 'HARPO'
+where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+#4d
+update actor
+set first_name = 'GROUCHO'
+where first_name = 'HARPO' and last_name = 'WILLIAMS';
+#5a
+show create table address;
+#6a
+select first_name,last_name,address.address
+from staff
+inner join address on staff.address_id = address.address_id;
+#6b
+select first_name,last_name,sum(payment.amount)
+from staff
+inner join payment on staff.staff_id = payment.staff_id
+where payment_date between '2005-08-01 00:00:00' and '2005-09-01 00:00:00' and payment.staff_id = 1
+or payment.staff_id = 2
+group by payment.staff_id;
+#6c
+select count(distinct actor_id),title
+from film_actor
+inner join film on film_actor.film_id = film.film_id
+group by film.film_id;
+#6d
+select count(inventory.film_id),film.title
+from inventory
+inner join film on inventory.film_id = film.film_id
+where film.title = 'Hunchback Impossible';
+#6e
+select first_name,last_name,sum(payment.amount)
+from customer
+inner join payment on customer.customer_id = payment.customer_id
+group by payment.customer_id
+order by last_name;
+#7a
